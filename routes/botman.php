5be9af0987323fa6aa851699aa6977f67cb2a100
1 <abdounovich@gmail.com>
2020-07-23 21:34:36 +0200

    'comment'

--- routes/botman.php
+++ routes/botman.php
@@ -1,25 +1,9 @@
 <?php
-use BotMan\BotMan\BotManFactory;
-use BotMan\BotMan\Drivers\DriverManager;
 use App\Http\Controllers\BotManController;
 
-
-
-$config = [
-    'facebook' => [
-        'token' => 'EAAW2ZByvfRBsBACTZBUqZBYOEpfuSULGK6xGyjv5E5PJHahBhKZATtfCr0fjD2GRT63tACtU0TyCy9q1pgWvr0sqTpybCM6xTFK9Y1CovfCUFDnEY11ZCQkTidNjDsMnswMaC7j8IDnxvfxsU0JZBiCcZCgn9xZCXImwodCtfx7iM4MMRcAeWH2qEvCwFPZApvwQZD',
-      'app_secret' => 'ask_123',
-      'verification'=>'ask_123',
-  ]
-];
-
-DriverManager::loadDriver(\BotMan\Drivers\Facebook\FacebookDriver::class);
-
-// Create BotMan instance
-BotManFactory::create($config);
 $botman = resolve('botman');
 
-$botman->hears('1', function ($bot) {
+$botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
