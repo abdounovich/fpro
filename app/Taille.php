1e51ab00675a1fe98606e0e4a5506de0f01112c5
1 <abdounovich@gmail.com>
2020-07-29 21:56:51 +0200

    'comment'

--- app/Taille.php
+++ app/Taille.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 
 class Taille extends Model
-{    public $timestamps = false;
+{    
      protected $fillable = ['produit_id'];
 
     public function product()
