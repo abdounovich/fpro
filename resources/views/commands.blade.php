bd179ca4a737f014cb6051627787377992a807e5
1 <abdounovich@gmail.com>
2020-08-01 01:24:29 +0200

    'comment'

--- resources/views/commands.blade.php
+++ resources/views/commands.blade.php
@@ -66,7 +66,7 @@
             <input type="hidden" name="type" value="2" class="form-control" placeholder="Name">
 
           </form>
-    
+    <p></p>
                 <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                   <button type="submit" class="btn btn-success">Confirmer </button>
                 
