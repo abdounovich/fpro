6b4a5dbc7778705664eedc6deba1108612758e31
1 <abdounovich@gmail.com>
2020-08-01 02:16:06 +0200

    'comment'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,45 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
       
     
-  
+        <style>
+            body {
+                font-family: "Varela Round", sans-serif;
+                margin: 0;
+                padding: 0;
+                background: radial-gradient(#57bfc7, #45a6b3);
+            }
+    
+            .container {
+                display: flex;
+                height: 100vh;
+                align-items: center;
+                justify-content: center;
+            }
+    
+            .content {
+                text-align: center;
+            }
+    
+            .logo {
+                margin-right: 40px;
+                margin-bottom: 20px;
+            }
+    
+            .links a {
+                font-size: 1.25rem;
+                text-decoration: none;
+                color: white;
+                margin: 10px;
+            }
+    
+            @media all and (max-width: 500px) {
+    
+                .links {
+                    display: flex;
+                    flex-direction: column;
+                }
+            }
+        </style>
     </head>
     <body>
         <div class="container">
